233d90d6dc2eaacde1a5461f5e0337bf49e3a53d
Rian <riansantosjr11@gmail.com>
2025-01-31 12:10:26 -0300

    Refactor error handling and improve error messages in product controller

--- src/Controller/productController.js
+++ src/Controller/productController.js
@@ -6,9 +6,9 @@ router.post('/', async (req, res) => {
   try {
     const product = await ProductRepository.createProduct(req.body);
     res.status(201).json(product);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-    console.log(err);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log(error);
   }
 })
 
@@ -17,25 +17,25 @@ router.get('/', async (req, res) => {
     const allProduct = await ProductRepository.findAllProducts();
     res.status(200).json(allProduct);
   } catch (error) {
-    res.status(500).json({ message: err.message });
-    console.log(err);
+    res.status(500).json({ message: error.message });
+    console.log(error);
   }
 })
 
 router.get('/:id', async (req, res) => {
   try {
     const productById = await ProductRepository.findProductById(req.params.id);
-    productById.length > 0 ? res.status(200).json(productById[0]) : res.status(404).send({ message : 'Product not found' });
+    productById.length > 0 ? res.status(200).json(productById[0]) : res.status(404).json({ message : 'Produto não encontrado' });
   } catch (error) {
-    res.status(500).json({ message: err.message });
-    console.log(err);
+    res.status(500).json({ message: error.message });
+    console.log(error);
   }
 })
 
 router.delete('/:id', async (req, res) => {
   try {
     const productDeleted = await ProductRepository.deleteProductById(req.params.id);
-    productDeleted.rowCount > 0 ? res.status(204).end() : res.status(404).json({ message : 'Product not found' });
+    productDeleted.rowCount > 0 ? res.status(204).end() : res.status(404).json({ message : 'Produto não encontrado' });
   } catch (error) {
     res.status(500).json({ message: error.message });
     console.log(error);
