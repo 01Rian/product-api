9a2a581fb1eafddfe397f43b00bad9619a2b274a
Rian <riansantosjr11@gmail.com>
2025-02-02 19:31:56 -0300

    Apply validation and formatting middleware to product routes

--- src/Controller/productRouter.js
+++ src/Controller/productRouter.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const ProductController = require('./productController');
+const { 
+  validateProductFields, 
+  validateProductId, 
+  sanitizeProductData,
+  formatDates,
+  formatDateResponse
+} = require('../Middleware/productValidationMiddleware');
 const router = express.Router();
 
-router.post('/', ProductController.createProduct);
+// Aplica o middleware de formatação de datas em todas as rotas
+router.use(formatDateResponse);
+
+router.post('/', validateProductFields, sanitizeProductData, formatDates, ProductController.createProduct);
 router.get('/', ProductController.getAllProducts);
-router.get('/:id', ProductController.getProductById);
-router.delete('/:id', ProductController.deleteProduct);
-router.put('/:id', ProductController.updateProduct);
+router.get('/:id', validateProductId, ProductController.getProductById);
+router.delete('/:id', validateProductId, ProductController.deleteProduct);
+router.put('/:id', validateProductId, validateProductFields, sanitizeProductData, formatDates, ProductController.updateProduct);
 
 module.exports = router;
